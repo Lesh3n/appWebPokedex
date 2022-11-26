dcdc6f3b3fefcdcd6cf88de4003468ff3209ba86
Andres Ibanez <andytimy@gmail.com>
2022-11-26 19:23:12 -0300

    Formulario hecho con modelForms, funcional. Las validaciones no se muestran en template.

--- pokemon/views.py
+++ pokemon/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from pokemon.models import Pokemon
 from . import forms
 from pokemon.forms import formulario_registro_pokemon
+from django.contrib import messages
 # Create your views here.fffff
 '''
 Esto sirvio en un inicio para probar si la web funcionaba correctamente.
@@ -25,33 +26,44 @@ TODO: Cambiar nombres a variables que son confusos a simple vista, se requiere a
 
 
 
-def renderTemplateRegistro(request):
-    return render(request, 'templatesPokedex/registroPkmn.html')
+def renderListaPkmn(request):
+    return render(request, 'templatesPokedex/listarPkmn.html')
 
-def formPokemon(request):
-    form = forms.formulario_registro_pokemon()
-
-    if request.method == 'POST':
-        form = forms.formulario_registro_pokemon(request.POST)
-        if form.is_valid():
-            nombre = form.cleaned_data['nombre']
-            tipo = form.cleaned_data['tipo']
-            ataque_base = form.cleaned_data['ataque_base_1']
-            ataque_especial = form.cleaned_data['ataque_especial_1']
-            dano_base = form.cleaned_data['dano_base']
-            defensa_base = form.cleaned_data['defensa_base']
-            '''
-            Se debe de poner el nombre del campo que esta presente en el model para luego poder ingresar la variable que recupera el dato ingresado por el usuario
-            '''
-            Pokemon.objects.create(nombre=nombre,tipo=tipo,ataque_base_1=ataque_base,ataque_especial_1=ataque_especial,dano_base=dano_base,defensa_base=defensa_base)
-
-    data = {'form' : form}
-    return render(request, 'templatesPokedex/ingresoPkmn.html', data)
 
 def datosPokemon(request):
     pokemons = Pokemon.objects.all()
     data = {'pokemons' : pokemons}
-    return render(request, 'templatesPokedex/registroPkmn.html', data)
+    return render(request, 'templatesPokedex/listarPkmn.html', data)
+
+
+def agregarPokemon(request):
+    form = forms.formulario_registro_pokemon()
+    if request.method == 'POST':
+        form = forms.formulario_registro_pokemon(request.POST)
+        if form.is_valid():
+            form.save()
+            '''
+            Es como la forma mas "arcaica" de ingresar los datos, pero, al hacer esto no se puede hacer lo de eliminar y actualizar datos no se pq.
+            '''
+            #nombre = form.cleaned_data['nombre']
+            #tipo = form.cleaned_data['tipo']
+            #ataque_base = form.cleaned_data['ataque_base_1']
+            #ataque_especial = form.cleaned_data['ataque_especial_1']
+            #dano_base = form.cleaned_data['dano_base']
+            #defensa_base = form.cleaned_data['defensa_base']
+            '''
+            Se debe de poner el nombre del campo que esta presente en el model para luego poder ingresar la variable que recupera el dato ingresado por el usuario
+            '''
+            #Pokemon.objects.create(nombre=nombre,tipo=tipo,ataque_base_1=ataque_base,ataque_especial_1=ataque_especial,dano_base=dano_base,defensa_base=defensa_base)
+            messages.success(request, "Datos ingresados exitosamente")
+            return datosPokemon(request)
+        else:
+            form = formulario_registro_pokemon()
+
+    data = {'form' : form}
+    return render(request, 'templatesPokedex/ingresoPkmn.html', data)
+
+
 
 def eliminarPokemon(request, id):
     pokemon = Pokemon.objects.get(id = id)
@@ -66,7 +78,11 @@ def actualizarPokemon(request,id):
         form = formulario_registro_pokemon(request.POST, instance=pokemon)
         if form.is_valid():
             form.save()
-        return renderTemplateRegistro(request)
+            messages.success(request, "Datos actualizados exitosamente")
+            return redirect('/')
+        #return render(request, 'templatesPokedex/listarPkmn.html')
+        else:
+            form = formulario_registro_pokemon()
     data = {'form' : form}
     return render(request, 'templatesPokedex/ingresoPkmn.html', data)
 
